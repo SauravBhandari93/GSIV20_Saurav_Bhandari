30fc04c46e699841dea522a17227fe2e31568476
SauravBhandari93 <83832259+SauravBhandari93@users.noreply.github.com>
2022-02-20 02:16:32 +0530

    Styling done

--- movies-browser/src/Components/App/App.js
+++ movies-browser/src/Components/App/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../Home/Home";
 import Movie from "../Movie/Movie";
 import NotFound from "../Elements/NotFound/NotFound";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:movieId" element={<Movie />} exact />
-          <Route element={<NotFound />} />
-        </Routes>
-      </>
+    <BrowserRouter basename="/ReactMovieDB">
+      <React.Fragment>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/:movieId" component={Movie} exact />
+          <Route component={NotFound} />
+        </Switch>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
--- movies-browser/src/Components/Elements/Navigation/Navigation.js
+++ movies-browser/src/Components/Elements/Navigation/Navigation.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HomeSharpIcon from "@mui/icons-material/HomeSharp";
 import "./Navigation.css";
 
 const Navigation = (props) => {
   return (
     <div className="rmdb-navigation">
       <div className="rmdb-navigation-content">
-        <Link to="/">
-          <p>Home</p>
-        </Link>
-        <p>/</p>
         <p>{props.movie}</p>
+        <Link to="/">
+          <HomeSharpIcon className="HomeSharpIcon" sx={{ fontSize: 25 }} />
+        </Link>
       </div>
     </div>
   );
